ff52d4727fff261cfe50bef83961e05eda4e2ad7
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-07-25 21:51:28 -0500

    feat: add navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,36 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: `
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" routerLink="/">{{ title }}</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a routerLink="/1" class="nav-link" routerLinkActive="active"
+                >Simplest</a
+              >
+            </li>
+            <li class="nav-item">
+              <a routerLink="/2" class="nav-link" routerLinkActive="active"
+                >Party planner</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <div class="container">
       <h1 class="mt-4">Welcome to {{ title }}!</h1>
       <router-outlet></router-outlet>
@@ -10,5 +40,5 @@ import { Component } from "@angular/core";
   `,
 })
 export class AppComponent {
-  title = "labs-nested-forms";
+  title = "LabsNestedForms";
 }
